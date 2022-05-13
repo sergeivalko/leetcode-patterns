bf34f9b1c925e7d488c377347544d161cf227269
Sergei Valko <sergei.valko98@mail.ru>
2022-05-13 22:50:49 +0300

    resolve Permutation in String

--- leetcode/solutions/sliding-window.fs
+++ leetcode/solutions/sliding-window.fs
@@ -3,7 +3,7 @@
 [<AutoOpen>]
 module sliding_window =
     // 3. https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
-    let lengthOfLongestSubstring (str: string) =
+    let lengthOfLongestSubstring str =
         if System.String.IsNullOrWhiteSpace str then
             0
         else
@@ -29,3 +29,43 @@ module sliding_window =
                 high <- high + 1
             
             right - start + 1
+            
+
+    // 567. https://leetcode.com/problems/permutation-in-string/        
+    let checkInclusion (str1:string,  str2: string) =
+        if str1.Length > str2.Length then
+            false
+        else
+            let map1 = Array.zeroCreate 26
+            let map2 = Array.zeroCreate 26
+            let aValue = int 'a'
+            let matches (map1: int[], map2: int[]) =
+                let mutable needBreak = false
+                let mutable index = 0
+                
+                while index < 26 && not needBreak do
+                    if(map1[index] <> map2[index]) then
+                        needBreak <- true
+                    index <- index + 1
+                not needBreak && true
+                
+            for i in 0 .. str1.Length - 1 do
+                let index1 = int str1[i] - aValue
+                let index2 = int str2[i] - aValue
+                map1[index1]<- map1[index1] + 1
+                map2[index2]<- map2[index2] + 1
+            
+            let mutable index = 0
+            let mutable needBreak = false
+            
+            while index < (str2.Length - str1.Length) && not needBreak do
+                    if matches (map1, map2) then
+                        needBreak <- true
+                    else
+                        let index1 = int str2[index + str1.Length] - aValue
+                        let index2 = int str2[index] - aValue
+                        map2[index1] <- map2[index1] + 1
+                        map2[index2] <- map2[index2] - 1
+                    index <- index + 1
+            
+            matches (map1, map2)
